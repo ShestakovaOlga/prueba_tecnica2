29a00e5bc4cfe80ac464ac8a78e66c76d4c85047
Olga Shestakova <olgashestakova@Air-de-Olga.home>
2019-07-15 11:41:26 +0200

    p

--- in.js
+++ in.js
@@ -1,1 +1,13 @@
-import React from 'react'
+
+import React, { useState } from "react"
+import { render } from "react-dom"
+import { Main } from "./src/main";
+
+
+function App() {
+    return <div>
+        <Main />
+    </div>
+}
+
+render(<App></App>, document.querySelector(`#app`))
